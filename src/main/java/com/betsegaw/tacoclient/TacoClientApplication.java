f68ee3139fc2bc09bc520f991d2ab63f16a6d410
Betsegaw Lemma <betsegawlemma@gmail.com>
2019-01-24 04:54:29 +0300

    first commit

--- src/main/java/com/betsegaw/tacoclient/TacoClientApplication.java
+++ src/main/java/com/betsegaw/tacoclient/TacoClientApplication.java
@@ -1,13 +1,15 @@
 package com.betsegaw.tacoclient;
 
+import java.net.URI;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.client.Traverson;
 
 import com.betsegaw.tacoclient.domains.Ingredient;
-import com.betsegaw.tacoclient.domains.Taco;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,76 +21,20 @@ public class TacoClientApplication {
 		SpringApplication.run(TacoClientApplication.class, args);
 	}
 	
+
 	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	public Traverson traverson() {
+		return new Traverson(URI.create("http://localhost:8080/api"),MediaTypes.HAL_JSON);
 	}
-	
-	@Bean
-	public CommandLineRunner fetchTacos(TacoRestClient tacoRestClient) {
-		return args -> {
-			log.info("----------------GET----------------");
-			log.info("Getting Tacos By Id");
-			log.info("Tacos: "+tacoRestClient.getTacoById(4L));
-			log.info("Getting Recent Tacos");
-			Iterable<Taco> tacos = tacoRestClient.getRecentTacos();
-			log.info("Recent taco designs: ");
-			for(Taco taco: tacos) {
-				log.info("  -" + taco);
-			}
-		};
-	}
-	
-	@Bean
-	public CommandLineRunner fetchIngredients(TacoRestClient tacoRestClient) {
-		return args -> {
-			log.info("----------------GET----------------");
-			log.info("Getting Ingredient By Id");
-			log.info("Ingredient: "+tacoRestClient.getIngredientById(1L));
-			log.info("Getting All Ingredients");
-			Iterable<Ingredient> ingredients = tacoRestClient.getAllIngredients();
-			log.info("All ingredients: ");
-			for(Ingredient ingredient: ingredients) {
-				log.info("  -" + ingredient);
-			}
-		};
-	}
-	
-	
+ 
 	 @Bean
-	  public CommandLineRunner putAnIngredient(TacoRestClient tacoRestClient) {
+	  public CommandLineRunner traversonGetIngredients(TacoRestClient tacoRestClient) {
 	    return args -> {
-	      log.info("----------------------- PUT -------------------------");
-	      Ingredient before = tacoRestClient.getIngredientById(1L);
-	      log.info("BEFORE:  " + before);
-	      tacoRestClient.updateIngredient(new Ingredient(1L, "Flour Tortillas", Ingredient.Type.WRAP));
-	      Ingredient after = tacoRestClient.getIngredientById(1L);
-	      log.info("AFTER:  " + after);
-	    };
-	  }
-	
-	 @Bean
-	  public CommandLineRunner addAnIngredient(TacoRestClient tacoRestClient) {
-	    return args -> {
-	      log.info("----------------------- POST -------------------------");
-	      Ingredient chix = new Ingredient(0L, "Shredded Chicken", Ingredient.Type.PROTEIN);
-	      Ingredient chixAfter = tacoRestClient.createIngredient(chix);
-	      log.info("AFTER=1:  " + chixAfter);
-	      Ingredient beefFajita = new Ingredient(0L, "Beef Fajita", Ingredient.Type.PROTEIN);
-	      tacoRestClient.createIngredient(beefFajita);
-	      Ingredient shrimp = new Ingredient(0L, "Shrimp", Ingredient.Type.PROTEIN);
-	      tacoRestClient.createIngredient(shrimp);
-	    };
-	  }
-	 
-	 @Bean
-	  public CommandLineRunner deleteAnIngredient(TacoRestClient tacoRestClient) {
-	    return args -> {
-	      log.info("----------------------- DELETE -------------------------");
-	      // Please use existing id in the database to see if the delete work
-	      tacoRestClient.deleteIngredient(tacoRestClient.getIngredientById(51L));
-	      log.info("The ingredient has been deleted or does not exist");
-	     
+	      Iterable<Ingredient> ingredients = tacoRestClient.getAllIngredientsWithTraverson();
+	      log.info("----------------------- GET INGREDIENTS WITH TRAVERSON -------------------------");
+	      for (Ingredient ingredient : ingredients) {
+	        log.info("   -  " + ingredient);
+	      }
 	    };
 	  }
 }
--- src/main/java/com/betsegaw/tacoclient/TacoRestClient.java
+++ src/main/java/com/betsegaw/tacoclient/TacoRestClient.java
@@ -1,57 +1,33 @@
 package com.betsegaw.tacoclient;
 
-import java.util.List;
+import java.util.Collection;
 
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.client.Traverson;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.betsegaw.tacoclient.domains.Ingredient;
-import com.betsegaw.tacoclient.domains.Taco;
 
 @Service
 public class TacoRestClient {
 
-	private RestTemplate restTemplate;
+	private Traverson traverson;
 
-	public TacoRestClient(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
+	public TacoRestClient(Traverson traverson) {
+		this.traverson = traverson;
 	}
 	
-	// Rest Template with HETOAES Disabled
-	public Taco getTacoById(Long tacoId) {
-		return restTemplate.getForObject("http://localhost:8080/design/{id}", Taco.class,tacoId);
-	}
-	
-	public List<Taco> getRecentTacos(){
-		return restTemplate.exchange("http://localhost:8080/design/recent",HttpMethod.GET,null,
-				new ParameterizedTypeReference<List<Taco>>() {}).getBody();
-	}
-
-	public Ingredient getIngredientById(Long ingredientId) {
-	    return restTemplate.getForObject("http://localhost:8080/ingredients/{id}",
-	                             Ingredient.class, ingredientId);
-	  }
-
-	 public List<Ingredient> getAllIngredients() {
-		    return restTemplate.exchange("http://localhost:8080/ingredients",
-		            HttpMethod.GET, null, new ParameterizedTypeReference<List<Ingredient>>() {})
-		        .getBody();
-	 }
-	
-	 public void updateIngredient(Ingredient ingredient) {
-		    restTemplate.put("http://localhost:8080/ingredients/{id}",
-		          ingredient,ingredient.getId());
-	}
-	
-	 public Ingredient createIngredient(Ingredient ingredient) {
-		    return restTemplate.postForObject("http://localhost:8080/ingredients",
-		        ingredient, Ingredient.class);
-	}
-	
-	public void deleteIngredient(Ingredient ingredient) {
-		restTemplate.delete("http://localhost:8080/ingredients/{id}",ingredient.getId());
-	}
+	public Iterable<Ingredient> getAllIngredientsWithTraverson() {
+		
+		ParameterizedTypeReference<Resources<Ingredient>> ingredientType = 
+			new ParameterizedTypeReference<Resources<Ingredient>>() {};
+			
+			Resources<Ingredient> ingredientResources = 
+					traverson.follow("ingredients").toObject(ingredientType);
+			Collection<Ingredient> ingredients = ingredientResources.getContent();
+			
+			return ingredients;
+	}	
 
 }
